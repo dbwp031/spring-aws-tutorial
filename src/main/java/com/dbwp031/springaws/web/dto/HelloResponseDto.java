270793b9393a8de0c1c771524478b9764b90f8c4
Yuje Lee <dbwp031@khu.ac.kr>
2023-05-17 03:31:33 +0900

    first commit

---
+++ src/main/java/com/dbwp031/springaws/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.dbwp031.springaws.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
---
+++ src/test/java/com/dbwp031/springaws/SpringawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbwp031.springaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
